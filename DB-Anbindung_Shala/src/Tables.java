2ae8f037a2b0cc3b2e749ab9ef5e1e9d05252bdc
Burim2023 <151534825+Burim2023@users.noreply.github.com>
2024-10-16 15:04:40 +0200

    Update Tables.java

--- DB-Anbindung_Shala/src/Tables.java
+++ DB-Anbindung_Shala/src/Tables.java
@@ -6,5 +6,25 @@
  * by: Shala
  * date: 16.10.2024
  * */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Tables {
+    public static void selectAllFromPersonTable(Connection connection) {
+        String sql = "SELECT id, vname, name FROM t_person";
+        try (PreparedStatement p = connection.prepareStatement(sql);
+             ResultSet rs = p.executeQuery()) {
+            System.out.println("id	vname	name");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String vname = rs.getString("vname");
+                String name = rs.getString("name");
+                System.out.println(id + "	" + vname + "	" + name);
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Exception: " + e.getMessage());
+        }
+    }
 }
