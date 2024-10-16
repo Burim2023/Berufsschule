5bf76d6c92f8ed68f517d7628c97c4eb26489cf4
Burim2023 <151534825+Burim2023@users.noreply.github.com>
2024-10-16 15:04:11 +0200

    Update Connector.java

--- DB-Anbindung_Shala/src/Connector.java
+++ DB-Anbindung_Shala/src/Connector.java
@@ -11,7 +11,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connector {
-
     public static Connection getConnection(String url, String user, String password) throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }
